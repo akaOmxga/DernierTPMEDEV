f3b683e137409ba1325369d72fe8db876f70238b
Barbo Lorenzo <barbo.lorenzo@hotmail.fr>
2025-01-13 09:26:34 +0100

    Implémentation de commentaire sur la class Joueur

--- dernierTPMEDEV/src/main/java/com/mycompany/derniertpmedev/Joueur.java
+++ dernierTPMEDEV/src/main/java/com/mycompany/derniertpmedev/Joueur.java
@@ -5,18 +5,33 @@
 package com.mycompany.derniertpmedev;
 
 /**
- *
+ * Représente un joueur dans un jeu. Chaque joueur a une couleur associée,
+ * qui peut être utilisée pour déterminer ses pièces sur le plateau.
+ * 
  * @author barbo et simon
  */
 public class Joueur {
     
+    // Variable représentant la couleur du joueur.
+    // 1 : couleur blanche, 2 : couleur noire
     private int couleur;
     
+    /**
+     * Constructeur qui initialise la couleur du joueur.
+     * 
+     * @param couleur La couleur du joueur (1 pour blanc, 2 pour noir)
+     */
     Joueur(int couleur){
         this.couleur = couleur;
     }
     
+    /**
+     * Retourne la couleur du joueur.
+     * 
+     * @return La couleur du joueur (1 pour blanc, 2 pour noir)
+     */
     public int getCouleur(){
         return this.couleur;
     }
 }
+
